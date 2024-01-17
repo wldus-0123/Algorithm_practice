62d07b968100c0bc8abb0cf927a5bd23792af0b9
wldus-0123 <96jy@naver.com>
2024-01-17 15:01:45 +0900

    feat: Heap_Practice

---
+++ Practice.01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 7;
+            int d; // 과제 마감일까지 남은 일수
+            int w; // 과제 점수
+
+            PriorityQueue<int,int> test = new PriorityQueue<int, int> (N);
+           
+        }
+    }
+}
